0ec9544ce70d6df49d0d144350df727090170908
Michael Dales <mdales@mac.com>
2019-03-12 21:34:38 +0000

    WIP: refactor code a little, provide config for server values.

--- src/github.com/ContentMine/ScienceSourceReview/main.go
+++ src/github.com/ContentMine/ScienceSourceReview/main.go
@@ -15,159 +15,71 @@
 package main
 
 import (
-	"fmt"
+    "encoding/json"
+    "flag"
 	"log"
 	"net/http"
-	"strings"
+	"os"
 	"time"
 
-	pongo "github.com/flosch/pongo2"
 	"github.com/gorilla/mux"
-
-	"github.com/ContentMine/wikibase"
 )
 
-const WIKIBASE_URL = "http://sciencesource.wmflabs.org"
-const SPARQL_QUERY_URL = "http://sciencesource-query.wmflabs.org/proxy/wdqs/bigdata/namespace/wdq/sparql"
-
-const ARTICLE_LIST_QUERY_SPARQL = `
-SELECT ?res ?page_ID ?article_text_title WHERE {
-  ?res wdt:P3 wd:Q4.
-  OPTIONAL { ?res wdt:P25 ?page_ID. }
-  OPTIONAL { ?res wdt:P11 ?article_text_title. }
-}
-`
-
-const ANNOTATION_LIST_QUERY_SPARQL = `
-SELECT ?res ?ScienceSource_article_title ?term ?dictionary WHERE {
-  ?res wdt:P12 wd:%s.
-  ?annotation wdt:P19 ?res
-  OPTIONAL { ?res wdt:P20 ?ScienceSource_article_title. }
-  OPTIONAL { ?annotation wdt:P15 ?term. }
-  OPTIONAL { ?annotation wdt:P16 ?dictionary. }
-}
-`
-
-type ArticleInfo struct {
-	Title  string
-	PageID string
-	ItemID string
+type OauthToken struct {
+    Key string `json:"key"`
+    Secret string `json:"secret"`
 }
 
-type AnnotationInfo struct {
-	Dictionary string
-	Count      int
+type ServerConfig struct {
+    Address string `json:"address"`
+    OauthCredentials OauthToken `json:"oauth"`
+    WikibaseURL string `json:"wikibase_url"`
+    QueryServiceURL string `json:"queryservice_url"`
 }
 
-func (a ArticleInfo) RawItemID() string {
-	return strings.TrimPrefix(a.ItemID, "http://sciencesource.wmflabs.org/entity/")
-}
+func loadConfig(path string) (ServerConfig, error) {
 
-func getArticleList() ([]ArticleInfo, error) {
-
-	resp, err := wikibase.MakeSPARQLQuery(SPARQL_QUERY_URL, ARTICLE_LIST_QUERY_SPARQL)
+	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return ServerConfig{}, err
 	}
 
-	data := make([]ArticleInfo, len(resp.Results.Bindings))
-	i := 0
-	for _, binding := range resp.Results.Bindings {
-
-		a := ArticleInfo{
-			Title:  binding["article_text_title"].Value,
-			PageID: binding["page_ID"].Value,
-			ItemID: binding["res"].Value,
-		}
-		data[i] = a
-		i += 1
-	}
-
-	return data, nil
+	var config ServerConfig
+	err = json.NewDecoder(f).Decode(&config)
+	return config, err
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-
-	res, err := getArticleList()
-	if err != nil {
-		panic(err)
-	}
-
-	w.WriteHeader(http.StatusOK)
-
-	t := pongo.Must(pongo.FromFile("templates/home.html"))
-	err = t.ExecuteWriter(pongo.Context{"articles": res}, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+// Simple wrapper so we can provide server config to each call
+type callWrapper struct {
+    *ServerConfig
+    H func(*ServerConfig, http.ResponseWriter, *http.Request)
 }
 
-func getArticleAnnotationList(article_id string) (string, map[string]AnnotationInfo, error) {
-
-	resp, err := wikibase.MakeSPARQLQuery(SPARQL_QUERY_URL, fmt.Sprintf(ANNOTATION_LIST_QUERY_SPARQL, article_id))
-	if err != nil {
-		return "", nil, err
-	}
-
-	title := ""
-	annotations := make(map[string]AnnotationInfo, 0)
-
-	for _, binding := range resp.Results.Bindings {
-		if title == "" {
-			title = binding["ScienceSource_article_title"].Value
-		}
-		term := binding["term"].Value
-		if term == "" {
-			continue
-		}
-
-		a, ok := annotations[term]
-		if ok {
-			a.Count += 1
-		} else {
-			a = AnnotationInfo{
-				Dictionary: binding["dictionary"].Value,
-				Count:      1,
-			}
-		}
-		annotations[term] = a
-
-	}
-
-	return title, annotations, nil
-}
-
-func articleHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	title, annotations, err := getArticleAnnotationList(id)
-	if err != nil {
-		panic(err)
-	}
-
-	w.WriteHeader(http.StatusOK)
-
-	t := pongo.Must(pongo.FromFile("templates/article.html"))
-	err = t.ExecuteWriter(pongo.Context{"annotations": annotations, "title": title}, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
+func (cw callWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    cw.H(cw.ServerConfig, w, r)
 }
 
 func main() {
 
+	var config_path string
+	flag.StringVar(&config_path, "config", "config.json", "configuration file, required")
+	flag.Parse()
+
+	config, err := loadConfig(config_path)
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandler)
-	r.HandleFunc("/article/{id:Q[0-9]+}/", articleHandler)
+	r.Handle("/", callWrapper{&config, homeHandler})
+	r.Handle("/article/{id:Q[0-9]+}/", callWrapper{&config, articleHandler})
 
     r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "0.0.0.0:4242",
+		Addr:         config.Address,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
---
+++ src/github.com/ContentMine/ScienceSourceReview/views.go
@@ -0,0 +1,152 @@
+//   Copyright 2019 Content Mine Ltd
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package main
+
+import (
+	"fmt"
+    "net/http"
+	"strings"
+
+	pongo "github.com/flosch/pongo2"
+	"github.com/gorilla/mux"
+
+	"github.com/ContentMine/wikibase"
+)
+
+const ARTICLE_LIST_QUERY_SPARQL = `
+SELECT ?res ?page_ID ?article_text_title WHERE {
+  ?res wdt:P3 wd:Q4.
+  OPTIONAL { ?res wdt:P25 ?page_ID. }
+  OPTIONAL { ?res wdt:P11 ?article_text_title. }
+}
+`
+
+const ANNOTATION_LIST_QUERY_SPARQL = `
+SELECT ?res ?ScienceSource_article_title ?term ?dictionary WHERE {
+  ?res wdt:P12 wd:%s.
+  ?annotation wdt:P19 ?res
+  OPTIONAL { ?res wdt:P20 ?ScienceSource_article_title. }
+  OPTIONAL { ?annotation wdt:P15 ?term. }
+  OPTIONAL { ?annotation wdt:P16 ?dictionary. }
+}
+`
+
+type ArticleInfo struct {
+	Title  string
+	PageID string
+	ItemID string
+}
+
+type AnnotationInfo struct {
+	Dictionary string
+	Count      int
+}
+
+func (a ArticleInfo) RawItemID() string {
+	return strings.TrimPrefix(a.ItemID, "http://sciencesource.wmflabs.org/entity/")
+}
+
+func getArticleList(queryservice_url string) ([]ArticleInfo, error) {
+
+	resp, err := wikibase.MakeSPARQLQuery(queryservice_url, ARTICLE_LIST_QUERY_SPARQL)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]ArticleInfo, len(resp.Results.Bindings))
+	i := 0
+	for _, binding := range resp.Results.Bindings {
+
+		a := ArticleInfo{
+			Title:  binding["article_text_title"].Value,
+			PageID: binding["page_ID"].Value,
+			ItemID: binding["res"].Value,
+		}
+		data[i] = a
+		i += 1
+	}
+
+	return data, nil
+}
+
+func homeHandler(config *ServerConfig, w http.ResponseWriter, r *http.Request) {
+
+	res, err := getArticleList(config.QueryServiceURL)
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	t := pongo.Must(pongo.FromFile("templates/home.html"))
+	err = t.ExecuteWriter(pongo.Context{"articles": res}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getArticleAnnotationList(queryservice_url string, article_id string) (string, map[string]AnnotationInfo, error) {
+
+	resp, err := wikibase.MakeSPARQLQuery(queryservice_url, fmt.Sprintf(ANNOTATION_LIST_QUERY_SPARQL, article_id))
+	if err != nil {
+		return "", nil, err
+	}
+
+	title := ""
+	annotations := make(map[string]AnnotationInfo, 0)
+
+	for _, binding := range resp.Results.Bindings {
+		if title == "" {
+			title = binding["ScienceSource_article_title"].Value
+		}
+		term := binding["term"].Value
+		if term == "" {
+			continue
+		}
+
+		a, ok := annotations[term]
+		if ok {
+			a.Count += 1
+		} else {
+			a = AnnotationInfo{
+				Dictionary: binding["dictionary"].Value,
+				Count:      1,
+			}
+		}
+		annotations[term] = a
+
+	}
+
+	return title, annotations, nil
+}
+
+func articleHandler(config *ServerConfig, w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	title, annotations, err := getArticleAnnotationList(config.QueryServiceURL, id)
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	t := pongo.Must(pongo.FromFile("templates/article.html"))
+	err = t.ExecuteWriter(pongo.Context{"annotations": annotations, "title": title}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
